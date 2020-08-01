ff868468b65a25a71385c5202e212e2e845e2dfc
Anderson Miranda Vesguerber <avesguerber@gmail.com>
2020-08-01 18:32:57 -0300

    Initial commit

---
+++ app/Http/Controllers/api/DogController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\dog;
+
+class DogController extends Controller
+{
+   
+    public function index()
+    {
+        return dog::all();
+    }
+
+    public function store(Request $request)
+    {
+        Dog::create($request->all());
+    }
+
+    public function show($id)
+    {
+        return Dog::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $dog = Dog::findOrFail($id);
+        $dog->update($request->all());
+    }
+    
+    public function destroy($id)
+    {
+        $dog = Dog::findOrFail($id);
+        $dog->delete();
+    }
+}
